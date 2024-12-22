30e5427e8fc215110b0731e4f070695efd5d58c7
chirag0728 <chirag0728@gmail.com>
2024-12-22 13:24:34 -0600

    refactor(utils): Use macOS-compatible IP header parsing with struct ip

--- src/utils.c
+++ src/utils.c
@@ -6,6 +6,8 @@
  */
 
 #include "headers.h"
+#include <netinet/ip.h> // For IP header parsing
+#include <netinet/in.h> // For sockaddr_in structure
 
 /**
  * @brief Prints information about a captured packet.
@@ -17,19 +19,21 @@
  * @param size The size of the captured packet.
  */
 void print_packet_info(const unsigned char *buffer, int size) {
-    struct iphdr *iph = (struct iphdr *)(buffer + sizeof(struct ether_header));
+    // Cast the buffer directly to IP header
+    struct ip *iph = (struct ip *)(buffer);
     struct sockaddr_in source, dest;
 
     memset(&source, 0, sizeof(source));
     memset(&dest, 0, sizeof(dest));
 
-    source.sin_addr.s_addr = iph->saddr;
-    dest.sin_addr.s_addr = iph->daddr;
+    // Populate source and destination addresses
+    source.sin_addr = iph->ip_src;
+    dest.sin_addr = iph->ip_dst;
 
     printf("\n========== Packet Details ==========\n");
     printf("Source IP: %s\n", inet_ntoa(source.sin_addr));
     printf("Destination IP: %s\n", inet_ntoa(dest.sin_addr));
-    printf("Protocol: %d\n", iph->protocol);
+    printf("Protocol: %d\n", iph->ip_p);
     printf("Packet Size: %d bytes\n", size);
     printf("=====================================\n");
 }
