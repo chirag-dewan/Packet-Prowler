30598d10b4601c66de16b6b69553452a4e8dec78
chirag0728 <chirag0728@gmail.com>
2024-12-22 13:48:39 -0600

    feat(main.c): added capabilities of outputting resilts

--- src/main.c
+++ src/main.c
@@ -5,6 +5,10 @@
  * Ensures the program runs with root privileges and starts the packet sniffer.
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "headers.h"
 
 /**
@@ -16,18 +20,32 @@
  * @param argv Argument vector.
  * @return int Exit status (0 for success, 1 for error).
  */
+
+char output_file[256] = "output.txt";
+int max_packets = 0;
+
 int main(int argc, char *argv[]) {
-    if (geteuid() != 0) {
-        fprintf(stderr, "PacketProwler must be run as root.\n");
-        return 1;
+    int opt;
+
+    while ((opt = getopt(argc, argv, "o:n:")) != -1) {
+        switch (opt) {
+            case 'o':
+                snprintf(output_file, sizeof(output_file), "%s", optarg);
+                break;
+            case 'n':
+                max_packets = atoi(optarg);
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-o output_file] [-n max_packets]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
     }
 
-    printf("Starting PacketProwler...\n");
-    if (!start_sniffer()) {
-        fprintf(stderr, "Failed to start the packet sniffer.\n");
-        return 1;
-    }
+    printf("Output File: %s\n", output_file);
+    printf("Max Packets: %d\n", max_packets);
 
+    start_sniffer();
     return 0;
 }
+
 
